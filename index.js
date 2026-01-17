346d51f6b124bc3390e8f1de60fa8017212c6879
Mohd Alquama <mohdalquama1@gmail.com>
2026-01-17 19:25:53 +0530

    ok

---
+++ index.js
@@ -0,0 +1,19 @@
+// let arr =[9,9.22,"dvdv"]
+// console.log(arr);
+// // 6->0
+// // 8->1
+// // 2->2
+// // 66->3
+// // 9->4
+// // arr[3]=9000
+// // console.log(arr[3]);
+// console.log(arr);
+
+let x = 10;
+let y = 20;
+let z = x + y;
+x = x + y
+ x += y;
+console.log(z);
+console.log(x);
+
---
+++ y.php
@@ -0,0 +1,9 @@
+<?php
+// Process form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    echo "Username: " . htmlspecialchars($username) . "<br>";
+    echo "Password: " . htmlspecialchars($password);
+}
+?>
