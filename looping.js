5a23fb42cb3cc561982d6c7009131242d83af468
Mohd Alquama <mohdalquama1@gmail.com>
2026-01-21 20:07:29 +0530

    no

--- looping.js
+++ looping.js
@@ -1,10 +1,10 @@
 for(let i = 0 ; i < 5; i++) {
     console.log("Iteration number: " + i);
 }
-
+l//let i 0= 0; // init
 console.log("========================================");
 
-let count = 0;
+let count = 0; /// init
 while(count < 5) {
     console.log("Count is: " + count);
     count++;
@@ -12,7 +12,7 @@ while(count < 5) {
 
 console.log("========================================");
 
-let num = 0;
+let num = 0; // init
 do {
     console.log("Number is: " + num);
     num++;
