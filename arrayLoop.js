5a133d820e76d9accd677cfbef94ccfe99a7aa43
Mohd Alquama <mohdalquama1@gmail.com>
2026-01-23 19:24:54 +0530

    loop - array

---
+++ arrayLoop.js
@@ -0,0 +1,9 @@
+// let arr = [7,5,3,2,45]
+// for(let i = 0 ; i<arr.length ; i++){    
+//     console.log(arr[i]);   
+// }
+
+let arr = [2,4,3,5,1]
+for(let i = arr.length-1 ; i>=0 ; i--){
+    console.log(arr[i]);
+}
--- sting.js
+++ sting.js
@@ -11,5 +11,5 @@ doc.innerText += `\nLowercase: ${s.toLowerCase()}`;
 doc.innerText += `\nIndex of 'console': ${s.indexOf("console")}`;
 doc.innerText += `\nSlice (6, 11): ${s.slice(6, 11)}`;
 doc.innerText += `\nReplace 'string' with 'text': ${s.replace("string", "text")}`;
-doc.innerText += `\nCharacter at index 5: ${s.charAt(5)}`;
+doc.innerText += `\nCharacter at index 5: ${s.charAt(7)}`;
 doc.innerText += `\nSplit by spaces: ${s.split(" ")}`;
