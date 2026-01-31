e96f4558b175f92aed50e9aaa7fe90200b17b81c
Mohd Alquama <mohdalquama1@gmail.com>
2026-01-31 20:20:17 +0530

    masking

--- function.js
+++ function.js
@@ -1,8 +1,24 @@
-function looping(start=1, end=100) {
-  for (let i = start; i <= end; i++) {
-    console.log(i);
+// function looping(start=1, end=100) {
+//   for (let i = start; i <= end; i++) {
+//     console.log(i);
+//   }
+// }
+
+// looping();
+
+ const maskNumber = (num) => {
+  let result = "";
+
+  for (let i = 0; i < num.length; i++) {
+    if (i < num.length - 2) {
+      result = result + "x";
+    } else {
+      result = result + num[i];
+    }
   }
-}
 
-looping();
+  return result;
+};
 
+// use
+console.log(maskNumber("9026226199"));
