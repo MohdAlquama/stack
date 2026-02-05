b93fa9e7fa7c95305d19efc66c442805650da0b4
Mohd Alquama <mohdalquama1@gmail.com>
2026-02-05 11:17:07 +0530

    ok

---
+++ forEach.js
@@ -0,0 +1,17 @@
+// let arr = [1, 3, 3323, 2, 333 , { a: 1 }, 23];
+
+// arr.forEach(
+//     (element) => {
+//         console.log(element * 2);
+//     }
+// )
+
+
+let users = {
+    1: { name: "Alice", age: 25 },
+    2: { name: "Bob", age: 30 },
+    3: { name: "Charlie", age: 35 },
+    4 :[1,2,3,4]
+}
+
+console.log(users);
---
+++ testFetach.js
@@ -0,0 +1,7 @@
+fetch("https://jsonplaceholder.typicode.com/users").then(
+    (res)=>res.json()
+).then(
+    (data)=>console.log(data)
+).catch(e=>{
+    console.log("Error:", e)
+})
