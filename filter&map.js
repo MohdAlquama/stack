0456a477cd50686c6a9cea647ef0285166d44af9
Mohd Alquama <mohdalquama1@gmail.com>
2026-02-05 20:28:19 +0530

    ok

--- filter&map.js
+++ filter&map.js
@@ -1,9 +1,56 @@
-let arr = [1, 2, 3, 4, 5];
-let filteredAndMapped = arr
-  .filter(num => num % 2 === 0)
+// let arr = [1, 2, 3, 4, 5];
+// let filteredAndMapped = arr
+//   .filter(num => num % 2 === 0)
 
-console.log(filteredAndMapped);
+// console.log(filteredAndMapped);
 
-let mapout = arr.map(num => num % 2 === 0 ? 1 : 0);
+// let mapout = arr.map(num => num % 2 === 0 ? 1 : 0);
 
-console.log(mapout);
+// console.log(mapout);
+
+// let arr = [1, 2, 5, 4, 5];
+// let maping = arr.map(
+//     (currentValue , index , array) => {
+//         console.log(currentValue , "|" , index , "|" , array);
+        
+//     }
+// )
+// 1 | 0 | [ 1, 2, 5, 4, 5 ]
+// 2 | 1 | [ 1, 2, 5, 4, 5 ]
+// 5 | 2 | [ 1, 2, 5, 4, 5 ]
+// 4 | 3 | [ 1, 2, 5, 4, 5 ]
+// 5 | 4 | [ 1, 2, 5, 4, 5 ]
+// ternary operator
+// condition ? expressionIfTrue : expressionIfFalse;
+
+// 
+// let arr = [1, 2, 5, 4, 5];
+// let maping = arr.map(
+//     (currentValue , index , array) => {
+//         return currentValue % 2 === 0 ? "even" : "odd";
+//     }
+// )
+// console.log(maping);
+
+// let arr = [1, 2, 5, 4, 5];
+// let filtering = arr.filter(
+//     (currentValue , index , array) => {
+//         return currentValue % 2 === 0 ;
+//     }
+// )
+// console.log(filtering);
+
+
+// let arr = [1, 2, 5, 4, 5];
+// let filteredAndMapped= arr
+//     .filter(
+//         (currentValue , index , array) => {
+//             return currentValue % 2 === 0 ; // 2 , 4
+//         }
+//     ).map(cu=>cu*10)
+// console.log(filteredAndMapped);
+
+// let arr = [1, 2, 5, 4, 5];
+// let filteredAndMapped= arr.filter(currentValue =>  currentValue % 2 === 0 ).map(cu=>cu*10)
+// console.log(filteredAndMapped);
+
