d579065b1845951f8e69af77d7bc4838323191c5
Mohd Alquama <mohdalquama1@gmail.com>
2026-02-06 11:59:26 +0530

    dfb

--- filterQuestion.js
+++ filterQuestion.js
@@ -1,8 +1,9 @@
-let arr=[2,4,4,4,5,6,6,3];
-const uniqueArr = arr.filter((currentValue) => {
-  return arr.indexOf(currentValue) != arr.lastIndexOf(currentValue);
+// let arr=[2,4,4,4,5,6,6,3];
+// const uniqueArr = arr.filter((currentValue) => {
+//   return arr.indexOf(currentValue) != arr.lastIndexOf(currentValue);
 
 
-});
-console.log(uniqueArr);
+// });
+// console.log(uniqueArr);
 
+let str = "hello world";  // h w e r l d
