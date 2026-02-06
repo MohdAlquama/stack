5bce325204a67103425c0d27262c1b1254b7e368
Mohd Alquama <mohdalquama1@gmail.com>
2026-02-06 11:54:55 +0530

    pp

---
+++ filterQuestion.js
@@ -0,0 +1,8 @@
+let arr=[2,4,4,4,5,6,6,3];
+const uniqueArr = arr.filter((currentValue) => {
+  return arr.indexOf(currentValue) != arr.lastIndexOf(currentValue);
+
+
+});
+console.log(uniqueArr);
+
